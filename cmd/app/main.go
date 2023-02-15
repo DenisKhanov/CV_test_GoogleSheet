2fae5bc0181aa1c46fca9b1e32510cec80822e0d
DK <denis_hanov@hotmail.com>
2023-02-15 20:05:25 +0300

    Complite

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,20 +6,24 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
-	"log"
 )
 
 func main() {
 	// Загружаем страницу
-	doc, err := goquery.NewDocument("https://confluence.hflabs.ru/pages/viewpage.action?pageId=1181220999")
+	urlConfluence := "https://confluence.hflabs.ru/pages/viewpage.action?pageId=1181220999"
+	doc, err := goquery.NewDocument(urlConfluence)
 	if err != nil {
-		log.Fatalf("Unable to retrieve page: %v", err)
+		fmt.Printf("Unable to retrieve page: %v\n", err)
+		fmt.Scanln()
+		return
 	}
 
 	// Ищем таблицу на странице
 	table := doc.Find("table")
 	if table.Length() == 0 {
-		log.Fatalf("Unable to find table on page")
+		fmt.Println("Unable to find table on page")
+		fmt.Scanln()
+		return
 	}
 
 	// Создаем массив для хранения данных
@@ -45,21 +49,27 @@ func main() {
 	})
 
 	// Инициализируем клиента Google Sheets API
-	srv, err := sheets.NewService(context.Background(), option.WithCredentialsFile("polar-office-377708-b66a7701095e.json"))
+	pathKey := "./cmd/keys/polar-office-377708-b66a7701095e.json"
+	srv, err := sheets.NewService(context.Background(), option.WithCredentialsFile(pathKey))
 	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+		fmt.Printf("Unable to retrieve Sheets client: %v\n", err)
+		fmt.Scanln()
+		return
 	}
 
 	// Определяем ID таблицы
-	spreadsheetID := "11MxexsZ-8UZu7hJEUMZYU0itFp7oCRp9n2ZeNax4Eo8"
-
+	spreadsheetID := "1N34uWDj33UlkCazW2v-SFtJy5NE1HiS5TxWk8UwDang"
+	urlGoogleShets := "https://docs.google.com/spreadsheets/d/1N34uWDj33UlkCazW2v-SFtJy5NE1HiS5TxWk8UwDang/edit#gid=0"
 	// Записываем данные в таблицу
 	writeOption := "RAW"
-	resp, err := srv.Spreadsheets.Values.Update(spreadsheetID, "A1", &sheets.ValueRange{Values: data}).ValueInputOption(writeOption).Do()
+	_, err = srv.Spreadsheets.Values.Update(spreadsheetID, "A1", &sheets.ValueRange{Values: data}).ValueInputOption(writeOption).Do()
 	if err != nil {
-		log.Fatalf("Unable to write data to sheet: %v", err)
+		fmt.Printf("Unable to write data to sheet: %v\n", err)
+		fmt.Scanln()
+		return
 	}
 
-	fmt.Printf("%#v\n", resp)
+	fmt.Printf("Перенос c cайта %s\n в таблицу %s\n выполнен успешно!\nНажмите Enter для выхода из программы.", urlConfluence, urlGoogleShets)
+	fmt.Scanln()
 
 }
